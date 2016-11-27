3bbd055d85b42c07ccbc0a385fec46036831fb00
Andrew Walz <andrewjwalz@gmail.com>
2016-11-27 13:20:42 -0700

    Initial Commit

---
+++ Source/SwiftyDraw.swift
@@ -0,0 +1,200 @@
+/*Copyright (c) 2016, Andrew Walz. All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ 
+ 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ 3. All advertising materials mentioning features or use of this software must display the following acknowledgement: This product includes software developed by the SwiftyDraw Framework.
+ 4. Neither the name of the SwiftyDraw Framework nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ''AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+ DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY 
+ OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
+
+import UIKit
+
+
+public protocol SwiftyDrawViewDelegate {
+    func SwiftyDrawDidBeginDrawing(view: SwiftyDrawView)
+    func SwiftyDrawIsDrawing(view: SwiftyDrawView)
+    func SwiftyDrawDidFinishDrawing(view: SwiftyDrawView)
+    func SwiftyDrawDidCancelDrawing(view: SwiftyDrawView)
+}
+
+extension SwiftyDrawViewDelegate {
+    
+    func SwiftyDrawDidBeginDrawing(view: SwiftyDrawView) {
+        //optional
+    }
+    
+    func SwiftyDrawIsDrawing(view: SwiftyDrawView) {
+        //optional
+    }
+    
+    func SwiftyDrawDidFinishDrawing(view: SwiftyDrawView) {
+        //optional
+    }
+    
+    func SwiftyDrawDidCancelDrawing(view: SwiftyDrawView) {
+        //optional
+    }
+}
+
+open class SwiftyDrawView: UIView {
+    
+    public var lineColor              : UIColor   = UIColor.black
+    public var lineWidth              : CGFloat   = 10.0
+    public var lineOpacity            : CGFloat   = 1.0
+    public var drawingEnabled         : Bool      = true
+    public var delegate               : SwiftyDrawViewDelegate?
+    private var pathArray             : [Line]    = []
+    private var currentPoint          : CGPoint   = CGPoint()
+    private var previousPoint         : CGPoint   = CGPoint()
+    private var previousPreviousPoint : CGPoint   = CGPoint()
+    
+    private struct Line {
+        var path    : CGMutablePath
+        var color   : UIColor
+        var width   : CGFloat
+        var opacity : CGFloat
+        
+        init(path : CGMutablePath, color: UIColor, width: CGFloat, opacity: CGFloat) {
+            self.path    = path
+            self.color   = color
+            self.width   = width
+            self.opacity = opacity
+        }
+    }
+    
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = UIColor.clear
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.backgroundColor = UIColor.clear
+    }
+    
+    override open func draw(_ rect: CGRect) {
+        let context : CGContext = UIGraphicsGetCurrentContext()!
+        context.setLineCap(.round)
+
+        for line in pathArray {
+            context.setLineWidth(line.width)
+            context.setStrokeColor(line.color.cgColor)
+            context.setAlpha(line.opacity)
+            context.addPath(line.path)
+            context.beginTransparencyLayer(auxiliaryInfo: nil)
+            context.strokePath()
+            context.endTransparencyLayer()
+        }
+    }
+    
+    override open func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard drawingEnabled == true else {
+            return
+        }
+        
+        self.delegate?.SwiftyDrawDidBeginDrawing(view: self)
+        if let touch = touches.first as UITouch! {
+            setTouchPoints(touch, view: self)
+            let newLine = Line(path: CGMutablePath(), color: self.lineColor, width: self.lineWidth, opacity: self.lineOpacity)
+            newLine.path.addPath(createNewPath())
+            pathArray.append(newLine)
+        }
+    }
+    
+    override open func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard drawingEnabled == true else {
+            return
+        }
+        
+        self.delegate?.SwiftyDrawIsDrawing(view: self)
+        if let touch = touches.first as UITouch! {
+            updateTouchPoints(touch, view: self)
+            let newLine = createNewPath()
+            if let currentPath = pathArray.last {
+                currentPath.path.addPath(newLine)
+            }
+        }
+    }
+    
+    override open func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard drawingEnabled == true else {
+            return
+        }
+        
+        self.delegate?.SwiftyDrawDidFinishDrawing(view: self)
+    }
+    
+    override open func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard drawingEnabled == true else {
+            return
+        }
+        
+        self.delegate?.SwiftyDrawDidCancelDrawing(view: self)
+    }
+    
+    public func removeLastLine() {
+        if pathArray.count > 0 {
+            pathArray.removeLast()
+        }
+        setNeedsDisplay()
+    }
+    
+    public func clearCanvas() {
+        pathArray = []
+        setNeedsDisplay()
+    }
+    
+/********************************** Private Functions **********************************/
+    
+    private func setTouchPoints(_ touch: UITouch,view: UIView) {
+        previousPoint = touch.previousLocation(in: view)
+        previousPreviousPoint = touch.previousLocation(in: view)
+        currentPoint = touch.location(in: view)
+    }
+    
+    private func updateTouchPoints(_ touch: UITouch,view: UIView) {
+        previousPreviousPoint = previousPoint
+        previousPoint = touch.previousLocation(in: view)
+        currentPoint = touch.location(in: view)
+    }
+    
+    private func createNewPath() -> CGMutablePath {
+        let midPoints = getMidPoints()
+        let subPath = createSubPath(midPoints.0, mid2: midPoints.1)
+        let newPath = addSubPathToPath(subPath)
+        return newPath
+    }
+    
+    private func calculateMidPoint(_ p1 : CGPoint, p2 : CGPoint) -> CGPoint {
+        return CGPoint(x: (p1.x + p2.x) * 0.5, y: (p1.y + p2.y) * 0.5);
+    }
+    
+    private func getMidPoints() -> (CGPoint,  CGPoint) {
+        let mid1 : CGPoint = calculateMidPoint(previousPoint, p2: previousPreviousPoint)
+        let mid2 : CGPoint = calculateMidPoint(currentPoint, p2: previousPoint)
+        return (mid1, mid2)
+    }
+    
+    private func createSubPath(_ mid1: CGPoint, mid2: CGPoint) -> CGMutablePath {
+        let subpath : CGMutablePath = CGMutablePath()
+        subpath.move(to: CGPoint(x: mid1.x, y: mid1.y))
+        subpath.addQuadCurve(to: CGPoint(x: mid2.x, y: mid2.y), control: CGPoint(x: previousPoint.x, y: previousPoint.y))
+        return subpath
+    }
+    
+    private func addSubPathToPath(_ subpath: CGMutablePath) -> CGMutablePath {
+        let bounds : CGRect = subpath.boundingBox
+        let drawBox : CGRect = bounds.insetBy(dx: -2.0 * lineWidth, dy: -2.0 * lineWidth)
+        self.setNeedsDisplay(drawBox)
+        return subpath
+    }
+}
+
+
+
+    
---
+++ SwiftyDrawExample/SwiftyDrawExample/ViewController.swift
@@ -0,0 +1,156 @@
+/*Copyright (c) 2016, Andrew Walz. All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ 
+ 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ 3. All advertising materials mentioning features or use of this software must display the following acknowledgement: This product includes software developed by the SwiftyDraw Framework.
+ 4. Neither the name of the SwiftyDraw Framework nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ''AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
+
+import UIKit
+
+class ViewController: UIViewController, SwiftyDrawViewDelegate {
+    
+    var drawView : SwiftyDrawView!
+    var redButton : ColorButton!
+    var greenButton : ColorButton!
+    var blueButton : ColorButton!
+    var orangeButton : ColorButton!
+    var purpleButton : ColorButton!
+    var yellowButton : ColorButton!
+    var undoButton : UIButton!
+    var deleteButton : UIButton!
+    var lineWidthSlider : UISlider!
+    var opacitySlider : UISlider!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        drawView = SwiftyDrawView(frame: self.view.frame)
+        drawView.delegate = self
+        self.view.addSubview(drawView)
+        addButtons()
+        addSliders()
+    }
+    
+    func addButtons() {
+        redButton = ColorButton(frame: CGRect(x: 10, y: self.view.frame.height - 50, width: 40, height: 40), color: UIColor.red)
+        redButton.addTarget(self, action: #selector(colorButtonPressed(button:)), for: .touchUpInside)
+        self.view.addSubview(redButton)
+        
+        greenButton = ColorButton(frame: CGRect(x: 10, y: self.view.frame.height - 100, width: 40, height: 40), color: UIColor.green)
+        greenButton.addTarget(self, action: #selector(colorButtonPressed(button:)), for: .touchUpInside)
+        self.view.addSubview(greenButton)
+        
+        blueButton = ColorButton(frame: CGRect(x: 10, y: self.view.frame.height - 150, width: 40, height: 40), color: UIColor.blue)
+        blueButton.addTarget(self, action: #selector(colorButtonPressed(button:)), for: .touchUpInside)
+        self.view.addSubview(blueButton)
+        
+        orangeButton = ColorButton(frame: CGRect(x: 60, y: self.view.frame.height - 150, width: 40, height: 40), color: UIColor.orange)
+        orangeButton.addTarget(self, action: #selector(colorButtonPressed(button:)), for: .touchUpInside)
+        self.view.addSubview(orangeButton)
+        
+        purpleButton = ColorButton(frame: CGRect(x: 60, y: self.view.frame.height - 100, width: 40, height: 40), color: UIColor.purple)
+        purpleButton.addTarget(self, action: #selector(colorButtonPressed(button:)), for: .touchUpInside)
+        self.view.addSubview(purpleButton)
+        
+        yellowButton = ColorButton(frame: CGRect(x: 60, y: self.view.frame.height - 50, width: 40, height: 40), color: UIColor.yellow)
+        yellowButton.addTarget(self, action: #selector(colorButtonPressed(button:)), for: .touchUpInside)
+        self.view.addSubview(yellowButton)
+        
+        undoButton = UIButton(frame: CGRect(x: self.view.frame.width - 60, y: 30, width: 60, height: 30))
+        undoButton.setTitleColor(UIColor.black, for: UIControlState())
+        undoButton.setTitle("undo", for: UIControlState())
+        undoButton.addTarget(self, action: #selector(undo), for: .touchUpInside)
+        self.view.addSubview(undoButton)
+        
+        deleteButton = UIButton(frame: CGRect(x: self.view.frame.width - 60, y: 60, width: 60, height: 30))
+        deleteButton.setTitleColor(UIColor.black, for: UIControlState())
+        deleteButton.setTitle("delete", for: UIControlState())
+        deleteButton.addTarget(self, action: #selector(deleteDrawing), for: .touchUpInside)
+        self.view.addSubview(deleteButton)
+    }
+    
+    func addSliders() {
+        lineWidthSlider = UISlider(frame: CGRect(x: 120, y: self.view.frame.height - 50, width: 100, height: 40))
+        lineWidthSlider.minimumValue = 1.0
+        lineWidthSlider.maximumValue = 30.0
+        lineWidthSlider.setValue(10.0, animated: false)
+        lineWidthSlider.isContinuous = true
+        lineWidthSlider.addTarget(self, action: #selector(lineWidthSliderValueDidChange(sender:)), for: .valueChanged)
+        self.view.addSubview(lineWidthSlider)
+        
+        opacitySlider = UISlider(frame: CGRect(x: 120, y: self.view.frame.height - 80, width: 100, height: 40))
+        opacitySlider.minimumValue = 0.001
+        opacitySlider.maximumValue = 1.0
+        opacitySlider.setValue(1.0, animated: false)
+        opacitySlider.isContinuous = true
+        opacitySlider.addTarget(self, action: #selector(lineOpacitySliderValueDidChange(sender:)), for: .valueChanged)
+        self.view.addSubview(opacitySlider)
+    }
+    
+    func colorButtonPressed(button: ColorButton) {
+        drawView.lineColor = button.color
+    }
+    
+    func undo() {
+        drawView.removeLastLine()
+    }
+    
+    func deleteDrawing() {
+        drawView.clearCanvas()
+    }
+    
+    func lineWidthSliderValueDidChange(sender:UISlider!) {
+        drawView.lineWidth = CGFloat(sender.value)
+    }
+    
+    func lineOpacitySliderValueDidChange(sender:UISlider!) {
+        drawView.lineOpacity = CGFloat(sender.value)
+    }
+    
+    func SwiftyDrawDidBeginDrawing(view: SwiftyDrawView) {
+        print("Did begin drawing")
+        UIView.animate(withDuration: 0.5, animations: {
+            self.redButton.alpha = 0.0
+            self.blueButton.alpha = 0.0
+            self.greenButton.alpha = 0.0
+            self.orangeButton.alpha = 0.0
+            self.purpleButton.alpha = 0.0
+            self.yellowButton.alpha = 0.0
+            self.undoButton.alpha = 0.0
+            self.deleteButton.alpha = 0.0
+            self.lineWidthSlider.alpha = 0.0
+            self.opacitySlider.alpha = 0.0
+        })
+    }
+    
+    func SwiftyDrawIsDrawing(view: SwiftyDrawView) {
+        print("Is Drawing")
+    }
+    
+    func SwiftyDrawDidFinishDrawing(view: SwiftyDrawView) {
+        print("Did finish drawing")
+        UIView.animate(withDuration: 0.5, animations: {
+            self.redButton.alpha = 1.0
+            self.blueButton.alpha = 1.0
+            self.greenButton.alpha = 1.0
+            self.orangeButton.alpha = 1.0
+            self.purpleButton.alpha = 1.0
+            self.yellowButton.alpha = 1.0
+            self.undoButton.alpha = 1.0
+            self.deleteButton.alpha = 1.0
+            self.lineWidthSlider.alpha = 1.0
+            self.opacitySlider.alpha = 1.0
+        })
+    }
+    
+    func SwiftyDrawDidCancelDrawing(view: SwiftyDrawView) {
+        print("Did cancel")
+    }
+}
+
